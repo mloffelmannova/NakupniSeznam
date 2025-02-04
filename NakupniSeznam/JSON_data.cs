2b60e87bda0b2162a8cfdc74eda9405c52fa0ab1
Michaela Löffelmann <michaela.loffelmann@educhem.cz>
2025-02-04 13:56:21 +0100

    zmena_vt1

---
+++ NakupniSeznam/JSON_data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NakupniSeznam
+{
+    internal class JSON_data
+    {
+        public List<string> NakupniListek { get; set; }
+    }
+}
